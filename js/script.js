d3c81c092208d25bb1719ef2b12dadcec10ab926
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-04-20 16:39:24 +0300

    jhj

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let savatcha = 0
+let ring = document.getElementById("ring")
+
+
+
+function qushish() {
+    
+    savatcha = savatcha + 1
+    ring.innerText = savatcha
+}
+
+function ayirish() {
+    savatcha = savatcha - 1
+    ring.innerText = savatcha
+}
+
